5d2314cc647b1e9c14e14a894e8b0da717b4eeec
NigelGroen5 <nigel.groen5@gmail.com>
2025-08-04 21:34:35 -0400

    Add Python script version of notebook

---
+++ premyy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+matches = pd.read_csv("matches.csv", index_col=0)
+matches.head()
+
+
+# In[3]:
+
+
+import os
+os.getcwd()
+
+
+# In[ ]:
+
+
+
+
